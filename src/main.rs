32d66794abb17b500aed9b97b6a6439e5ee220b2
0xd34db33f <0xd34db33f@noreply.github.com>
2020-01-15 12:06:06 +0300

    update for vis

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,38 @@
-#[allow(dead_code)]
+extern crate rayon_logs as rayon;
+
 use clap::{App, Arg};
 use ssh2::{Session, Agent};
 use std::net::{TcpStream, ToSocketAddrs};
 use std::io::{Read, BufReader};
 use std::path::Path;
-use evmap;
 use std::io::prelude::*;
 use std::fs::File;
 use std::slice::{Chunks, ChunksMut};
-use std::thread::spawn;
+use rayon::prelude::*;
+use std::fmt::Display;
+use rayon::ThreadPoolBuilder;
 
-const NTHREADS: usize = 2;
 
-fn process_hosts(list: &[String])
-{
-    for host in list {
-        process_host(host);
-    }
-}
-
-fn process_host<A>(hostname: A)
-    where A: ToSocketAddrs {
-    let tcp = match TcpStream::connect(hostname) {
+fn process_host<A>(hostname: A) -> Result<String, String>
+    where A: ToSocketAddrs + Display {
+    let tcp = match TcpStream::connect(&hostname) {
         Ok(a) => a,
         Err(e) => {
-            return;
+            return Err(format!("{}:{}", hostname, e).to_string());
         }
     };
     let mut sess = match Session::new() {
         Ok(a) => a,
         Err(e) => {
             // todo logging
-            return;
+            return Err(format!("{}:{}", hostname, e).to_string());
         }
     };
     sess.set_tcp_stream(tcp);
     match sess.handshake() {
         Ok(a) => a,
         Err(e) => {
-            return;
+            return Err(format!("{}:{}", hostname, e).to_string());
         }
     };
 
@@ -46,30 +40,28 @@ fn process_host<A>(hostname: A)
     match sess.userauth_agent("scan") {
         Ok(a) => a,
         Err(e) => {
-            return;
+            return Err(format!("{}:{}", hostname, e).to_string());
         }
     };
     let mut channel = match sess.channel_session() {
         Ok(a) => a,
         Err(e) => {
-            return;
+            return Err(format!("{}:{}", hostname, e).to_string());
         }
     };
     channel.exec("uptime").unwrap();
     let mut s = String::new();
     match channel.read_to_string(&mut s) {
         Err(e) => {
-            return;
+            return Err(format!("{}:{}", hostname, e).to_string());
         }
         _ => ()
     };
-    println!("{}", s);
-    channel.wait_close();
-    println!("{}", channel.exit_status().unwrap());
+    Ok(format!("{}:{}", hostname, s))
 }
 
 fn hosts_builder(path: &Path) -> Vec<String> {
-    let  file = File::open(path)
+    let file = File::open(path)
         .expect("Unable to open the file");
     let reader = BufReader::new(file);
     reader.lines()
@@ -91,13 +83,11 @@ fn main() {
                 .takes_value(true)
         )
         .get_matches();
-    let hosts  = hosts_builder(Path::new(&args.value_of("config").unwrap()));
-    let hosts: Vec<&[String]> =hosts.chunks(NTHREADS).collect();
-    dbg!(&hosts);
-    let mut thread_heandles = vec![];
-    for chunk in hosts {
-        thread_heandles.push(
-            spawn(move || process_hosts(chunk)))
-    }
+    let hosts = hosts_builder(Path::new(&args.value_of("config").unwrap()));
+//    rayon::ThreadPoolBuilder::new().num_threads(1).build_global().unwrap();
+    let pool = ThreadPoolBuilder::new().num_threads(100).build().expect("failed creating pool");
+   pool.install(|| hosts.par_iter()
+        .map(|x| process_host(x))
+        .for_each(|x| println!("{:?}", x)));
 
 }
