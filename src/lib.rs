d4f5210d495ccc40f99d574d80455b041909efd6
0xd34db33f <0xd34db33f@noreply.github.com>
2020-07-29 11:30:12 +0300

    Rework of host processing part

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-use async_ssh2::{Error, PublicKey, Session};
+use anyhow::Error;
+use async_ssh2::{PublicKey, Session};
 use clap::{App, Arg};
 use humantime::format_duration;
 use serde::Serialize;
+use smol::Async;
 use std::fmt::Display;
 use std::fs;
 use std::fs::File;
@@ -9,125 +11,82 @@ use std::io::prelude::*;
 use std::io::{BufReader, Read};
 use std::net::{TcpStream, ToSocketAddrs};
 use std::path::Path;
-use std::sync::mpsc::{Receiver, SyncSender};
-use std::sync::{mpsc, Arc};
-use std::time::Instant;
-use tokio::prelude::*;
-use tokio::runtime::{Builder, Runtime};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio::sync::Semaphore;
 
 #[derive(Serialize, Debug, Clone)]
 pub struct Response {
     result: String,
     hostname: String,
-    process_time: String,
-    status: bool,
+    process_time: Duration,
 }
 
 async fn process_host<A>(
     hostname: A,
     command: Arc<String>,
-    tx: SyncSender<Response>,
+    mut tx: Sender<Response>,
     connection_pool: Arc<Semaphore>,
-) -> Response
+) {
+    let start_time = Instant::now();
+    let result = process_host_inner(&hostname, command, connection_pool).await;
+    let process_time = Instant::now() - start_time;
+    let response = match result {
+        Ok(a) => Response {
+            result: a,
+            hostname: hostname.into(),
+            process_time,
+        },
+        Err(e) => Response {
+            result: e.to_string(),
+            hostname: hostname.into(),
+            process_time,
+        },
+    };
+    if let Err(e) = tx.send(response.clone()).await {
+        eprintln!("Error sending result via channel: {}", e);
+    };
+}
+
+async fn process_host_inner<A>(
+    hostname: &A,
+    command: Arc<String>,
+    connection_pool: Arc<Semaphore>,
+) -> Result<String, Error>
 where
     A: ToSocketAddrs + Display,
 {
     let guard = connection_pool.acquire().await;
-    let start_time = Instant::now();
-    let tcp = match TcpStream::connect(&hostname) {
-        Ok(a) => a,
-        Err(e) => return construct_error(&hostname, start_time, e.to_string(), &tx),
-    };
-    dbg!(format!("Tcp connection intialized {}", &hostname));
-    let mut sess = match Session::new() {
-        Ok(a) => a,
-        Err(e) => return construct_error(&hostname, start_time, e.to_string(), &tx),
-    };
+    let sync_stream = TcpStream::connect(&hostname)?;
+    let tcp = Async::new(sync_stream)
+        .map_err(|e| format!("Error creating async TcpStream: {}", e).into())?;
+    let mut sess = Session::new().map_err(|e| format!("Error initializing session").into())?;
     const TIMEOUT: u32 = 6000;
     sess.set_timeout(TIMEOUT);
-    dbg!(format!("Session is built {}", &hostname));
-    if let Err(e) = sess.set_tcp_stream(tcp) {
-        return construct_error(&hostname, start_time, e.to_string(), &tx);
-    };
-    dbg!(format!("Tcp stream is set : {}", &hostname));
-
-    match sess.handshake().await {
-        Ok(a) => {
-            dbg!(format!("Handshake is done: {}", &hostname));
-            a
-        }
-        Err(e) => {
-            dbg!("Handhaske is failed");
-            return construct_error(&hostname, start_time, e.to_string(), &tx);
-        }
-    };
-    dbg!(format!("Handshake is done: {}", &hostname));
-    // Try to authenticate with the first identity in the agent.
-    let mut agent = match sess.agent() {
-        Ok(a) => a,
-        Err(e) => {
-            return construct_error(&hostname, start_time, e.to_string(), &tx);
-        }
-    };
-    match agent.connect().await {
-        Ok(_) => (),
-        Err(e) => {
-            return construct_error(&hostname, start_time, e.to_string(), &tx);
-        }
-    };
-    if let Err(e) = sess.userauth_agent("scan").await {
-        return construct_error(&hostname, start_time, e.to_string(), &tx);
-    };
-
-    let mut channel = match sess.channel_session().await {
-        Ok(a) => a,
-        Err(e) => {
-            return construct_error(&hostname, start_time, e.to_string(), &tx);
-        }
-    };
-    if let Err(e) = channel.exec(&command).await {
-        return construct_error(&hostname, start_time, e.to_string(), &tx);
-    };
-    let mut s = String::new();
-    if let Err(e) = channel.read_to_string(&mut s).await {
-        return construct_error(&hostname, start_time, e.to_string(), &tx);
-    };
-    let end_time = Instant::now();
-    let response = Response {
-        hostname: hostname.to_string(),
-        result: "".to_string(),
-        process_time: format_duration(end_time - start_time).to_string(),
-        status: true,
-    };
-    match tx.send(response.clone()) {
-        Ok(_) => (),
-        Err(e) => eprintln!("Error sending response {}", e),
-    };
-    response
-}
-
-fn construct_error<A>(
-    hostname: &A,
-    start_time: Instant,
-    e: String,
-    tx: &SyncSender<Response>,
-) -> Response
-where
-    A: Display + ToSocketAddrs,
-{
-    dbg!("Constructing error");
-    let response = Response {
-        result: e,
-        hostname: hostname.to_string(),
-        process_time: format_duration(Instant::now() - start_time).to_string(),
-        status: false,
-    };
-    match tx.send(response.clone()) {
-        Ok(_) => (),
-        Err(e) => eprintln!("Error sending response {}", e),
-    }
-    response
+    sess.set_tcp_stream(tcp);
+    sess.handshake()
+        .await
+        .map_err(|e| format!("Failed establishing handshake: {}", e).into())?;
+    let mut agent = sess
+        .agent()
+        .map_err(|e| format!("Failed connecting to agent: {}", e).into())?;
+    agent.connect().await?;
+    drop(guard); //todo test, that it really works
+    let mut channel = sess
+        .channel_session()
+        .await
+        .map_err(|e| format!("Failed openning channel: {}", e).into())?;
+    channel
+        .exec(&command)
+        .await
+        .map_err(|e| format!("Failed executing command in channel: {}", e).into())?;
+    let mut channel_buffer = String::with_capacity(4096);
+    channel
+        .stream(1)
+        .read_to_string(&mut channel_buffer)
+        .map_err(|e| format!("Error reading result of work: {}", e).into())?;
+    Ok(channel_buffer)
 }
 
 pub struct ParallelSshProps {
@@ -142,7 +101,6 @@ impl Default for ParallelSshProps {
 }
 
 impl ParallelSshProps {
-
     pub fn new(mut self, max_connections: usize) -> ParallelSshProps {
         self.maximum_connections = max_connections;
         self
@@ -153,16 +111,18 @@ impl ParallelSshProps {
         A: Display + ToSocketAddrs,
     {
         let num_of_threads = Arc::new(Semaphore::new(self.maximum_connections));
-        let (tx, rx): (SyncSender<Response>, Receiver<Response>) = mpsc::sync_channel(0);
+        let (tx, rx): (Sender<Response>, Receiver<Response>) =
+            channel(self.maximum_connections * 2); //todo check this
         hosts
             .into_iter()
             .map(|host| {
-                smol::Task::spawn( process_host(
+                smol::Task::spawn(process_host(
                     host.to_string(),
                     Arc::new(command.to_string()),
                     tx.clone(),
                     num_of_threads.clone(),
-                )).detach();
+                ))
+                .detach();
             })
             .collect();
         rx
